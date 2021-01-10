fc4ba4c834d1aa5d0652785ddc99bcfca853e707
Your Name <reyhanacynthia@gmail.com>
2021-01-10 16:45:25 +0300

    third commit

--- categorypick.js
+++ categorypick.js
@@ -9,44 +9,25 @@ ajax.send();
 
 ajax.onreadystatechange = function(){
   if (this.readyState == 4 && this.status == 200){
-
+    
       var data = JSON.parse(this.responseText);
           var html = "";
-    // .then(res => res.json()) // comment this out for now
-    // .then(res => res.text())          // convert to plain text
-    // .then(text => console.log(text)) 
-    // var data = JSON.parse(this.responseText);
+
     console.log(data);
+        html += "<tr ><th>Pick your categories</th></tr>";
 
-    for (var a = 0; a < data.length; a++){
-      var itemcategoryname = data[a].Item_category_name;
-
-
-      if (activation == 1){
-        
-        html = "<input type='checkbox' id='vehicle1' name='vehicle1' value='Bike'>";
-        html = "<label for='vehicle1'>"+itemcategoryname+"</label><br>";  
-
-        
+        for (var a = 0; a < data.length; a++){
+            html += "<tr><td><label class='container'><input type='checkbox' >"+data[a].Item_category_name+"<span class='checkmark'></span></label><br></td></td></tr>";  
         }
-      }
-document.getElementById("data").innerHTML = html;
+        html += "<a href='index.html'><input style='color: #2F4F4F; float: right; margin: 15px;' class='button' name='submit' type='submit' value='submit' class='panel-footer'></a>";
+
+       document.getElementById("data").innerHTML = html;
+    }   
+    
     }
-  }
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("data");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      } 
-    }
+
+
+
+
+
   
--- categorypick.php
+++ categorypick.php
@@ -7,7 +7,7 @@ if(!$conn)
     echo 'Database Error: ' . mysqli_connect_error() ;
     exit;
 }
-$sql = mysqli_query($conn, "select Item_category_name, ativation from item_category");
+$sql = mysqli_query($conn, "SELECT item_category.Item_category_name, item_category.activation, item_category.Item_category_description, item_category.Item_category_ID FROM item_category where item_category.activation = 1");
 $data = array();
 while($row = mysqli_fetch_assoc($sql))
 {
